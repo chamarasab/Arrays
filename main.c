304cac8c81dffd28e4bba5d30760939067d56614
chamarasab <chamara.sab@outlook.com>
2021-11-14 10:11:38 +0530

    commenting each line's function

--- main.c
+++ main.c
@@ -3,11 +3,21 @@
 int main() {
     printf("Hello, Arrays!\n");
 
-    int numbers[100];                           //array declaration.
+   /* int numbers[100];                           //array declaration.
 
     for (int i = 0; i < 100; ++i) {              //assigning and printing values to array using for loop.
         numbers[i] = i+1;                       //values assigning using loop iteration value.
         printf("%d,",numbers[i]);       //print all values inside the array.
     }
+    */
+
+   int arr[5];
+   arr[0] = 5;
+   arr[3/2] = -10;
+   arr[2] = 2;
+   arr[3] = arr[0];
+
+    printf("%d, %d, %d, %d",arr[0],arr[1],arr[2],arr[3]);
+
     return 0;
 }
